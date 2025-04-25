958a301728dce145d777870227238b7e906cd3c6
Nawfal <134062420+xFalzz@users.noreply.github.com>
2025-04-25 12:14:59 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -57,7 +57,7 @@ const Navbar = () => {
 			{/* Dekstop */}
 			<div className="flex justify-between relative top-3 hidden lg:flex">
 				<div>
-					<img src="public/Logo MM.png" className="w-12 h-12 rounded-full" alt="" />
+					<img src="/Logo MM.png" className="w-12 h-12 rounded-full" alt="" />
 				</div>
 				<ul className="mt-2 flex gap-5">
 					<li className="mb-4">
